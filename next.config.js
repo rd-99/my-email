f8473f879133aea93938d517fe738c95d4c2bd59
rd-99 <ruturajd1999@gmail.com>
2025-04-06 11:36:15 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
+// /**
+//  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+//  * for Docker builds.
+//  */
+// import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+// /** @type {import("next").NextConfig} */
+// const config = {};
 
-export default config;
+// export default config;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          hostname: "lh3.googleusercontent.com",
+        },
+      ],
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    reactStrictMode: false,
+  };
+  
+  export default nextConfig;
